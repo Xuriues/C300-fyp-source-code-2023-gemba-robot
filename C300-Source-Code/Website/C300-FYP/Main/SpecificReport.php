4de87acf9702c1ce1c23bbfcd9144a98418e8628
Xuriues <xuriusgaming@gmail.com>
2023-08-06 16:23:28 +0800

    Final update

--- C300-Source-Code/Website/C300-FYP/Main/SpecificReport.php
+++ C300-Source-Code/Website/C300-FYP/Main/SpecificReport.php
@@ -89,7 +89,7 @@ function GoBackWithRefresh(event) {
  }
 
 </script>
-<body class="mainPageContainer">
+<body class="SpecificControllerPage">
 	<?php include "navPartialView.php"; ?>
 		<div id="tableContainer">
 		<?php
@@ -98,65 +98,64 @@ function GoBackWithRefresh(event) {
 		?>
 		<h1 style="margin-left:20px">Report Details</h1>
 		<form method="POST" action="UpdateReportFunc.php">
-    <input type="hidden" name="foundReport" value="<?php echo htmlspecialchars(json_encode($foundReport)); ?>">
-				<div class="image-container">
-					<img src="<?php echo $urlImg; ?>" style="width: 500px; height: 500px;"> 
-					<div class="label-container">
-						<div>
-							<label> <?php echo $errorMsg; ?> </label>
-						</div>
-						<div>
-							<label>Date Of Incident: <?php echo $foundReport["date"]; ?> </label>
-						</div>
-						<div>
-							<label>Time Of Incident: <?php echo $foundReport["time"]; ?> </label>
-						</div>
-						<div>
-							<label>Description: <?php echo $foundReport["Description"]; ?> </label>
-						</div>
-						<?php 
-								if ($foundReport["ReasonForClosure"] != null) {
-									
-										echo "<div>"; 
-									 	echo "<label>Reason for closure: " . $foundReport["ReasonForClosure"] ."</label>";
-										echo "</div>";
-
-										echo "<div>"; 
-									 	echo "<label>Date & Time of Closure: " . $foundReport["ClosureDateTime"] ."</label>";
-										echo "</div>";
-								}
-								else {
-									echo '
-										<div class="select-box">
-										    <label for="select-box1" class="label select-box1" style="font-size: 19px;">
-										        <span class="label-desc">Reason for Closure</span>
-										    </label>
-										    <select id="select-box1" class="select" name="reasonForClosure">
-										        <option disabled selected>Select an option</option>
-										        <option value="Incident resolved">Incident resolved</option>
-										        <option value="False Detection">False Detection</option>
-										        <option value="Repeated Entry">Repeated Entry</option>
-										    </select>
-										</div>';
-								}
-              			?>
-						<div>
-							<div><label>Additional Notes for closure:</label></div>
-							<textarea id="incidentTextArea" name="textAreaReport" <?php if (!$foundReport["Report_Status"]) { echo "disabled"; } ?>><?php if (!$foundReport["Report_Status"]) { echo $foundReport["AdditionalInfo"]; } ?></textarea>
-						</div>
-						<div class="reportPageBtnContainer">
-							<?php 
-								 if ($foundReport["Report_Status"]) {
-								 		echo "<input type='submit' value='Close Incident'>";
-								}
-							?>
-
-							<a href="#" onclick="GoBackWithRefresh();return false;"> 
-								<input type="button" value="Back">
-							</a>
-						</div>		
+    	<input type="hidden" name="foundReport" value="<?php echo htmlspecialchars(json_encode($foundReport)); ?>">
+			<div class="image-container">
+				<img id="imgSpecific" src="<?php echo $urlImg; ?>" > 
+				<div class="label-container">
+					<div>
+						<label> <?php echo $errorMsg; ?> </label>
 					</div>
+					<div>
+						<label>Date Of Incident: <?php echo $foundReport["date"]; ?> </label>
+					</div>
+					<div>
+						<label>Time Of Incident: <?php echo $foundReport["time"]; ?> </label>
+					</div>
+					<div>
+						<label>Description: <?php echo $foundReport["Description"]; ?> </label>
+					</div>
+					<?php 
+						if ($foundReport["ReasonForClosure"] != null) {
+							echo "<div>"; 
+							echo "<label>Reason for closure: " . $foundReport["ReasonForClosure"] ."</label>";
+							echo "</div>";
+
+							echo "<div>"; 
+							echo "<label>Date & Time of Closure: " . $foundReport["ClosureDateTime"] ."</label>";
+							echo "</div>";
+						}
+						else {
+							echo '
+								<div class="select-box">
+									<label for="select-box1" class="label select-box1" style="font-size: 19px;">
+							        <span class="label-desc">Reason for Closure</span>
+									</label>
+									<select id="select-box1" class="select" name="reasonForClosure">
+										<option disabled selected>Select an option</option>
+										<option value="Incident resolved">Incident resolved</option>
+										<option value="False Detection">False Detection</option>
+										<option value="Repeated Entry">Repeated Entry</option>
+									</select>
+								</div>';
+						}
+              		?>
+					<div>
+						<div><label>Additional Notes for closure:</label></div>
+						<textarea id="incidentTextArea" name="textAreaReport" <?php if (!$foundReport["Report_Status"]) { echo "disabled"; } ?>><?php if (!$foundReport["Report_Status"]) { echo $foundReport["AdditionalInfo"]; } ?></textarea>
+					</div>
+					<div class="reportPageBtnContainer">
+						<?php 
+							if ($foundReport["Report_Status"]) {
+								echo "<input type='submit' value='Close Incident'>";
+							}
+						?>
+
+						<a href="#" onclick="GoBackWithRefresh();return false;"> 
+							<input type="button" value="Back">
+						</a>
+					</div>		
 				</div>
-			</form>
+			</div>
+		</form>
 </body>
 </html>
